a948cc3fcd150a39f930d3d7ca47914a242ca7a3
Tristan MENARD <93603853+TRISTANO13@users.noreply.github.com>
2023-12-02 21:42:21 -0500

    Add files via upload

---
+++ data/data.js
@@ -0,0 +1,33 @@
+const data = [
+    {
+      title: "Octane",
+      desc: "The Octane is a vehicle body released on July 7, 2015, along with the game release. It originally appeared in the Rocket League precursor Supersonic Acrobatic Rocket-Powered Battle-Cars. The Octane is classified as Common rarity.",
+      img: "https://static.wikia.nocookie.net/rocketleague/images/f/f1/Octane_body_icon.png/revision/latest?cb=20170526223331s"
+    },
+    {
+      title: "Dominus",
+      desc: "The Dominus is a vehicle released on August 13, 2015, which could only be obtained as DLC from the Supersonic Fury DLC Pack before it was made unlockable for free without purchase. The Dominus hitbox, used by other cars, is based off of it.",
+      img: "https://static.wikia.nocookie.net/rocketleague/images/2/2b/Dominus_body_icon.png/revision/latest?cb=20170529160106"
+    },
+    {
+      title: "Masamune",
+      desc: "Masamune is a battle-car released on July 5, 2016, as part of the Rocket League Collector's Edition.It was made available to purchase separately on July 18, 2016.",
+      img: "https://static.wikia.nocookie.net/rocketleague/images/5/53/Masamune_body_icon.png/revision/latest?cb=20170601162653"
+    },
+    {
+      title: "Fennec",
+      desc: "Fennec is a vehicle body that was released on July 1, 2019, which could originally be obtained from the Totally Awesome Crate. As of today, since Crates are retired, it can be obtained from Blueprints, Trade-ins or from Item Shop. It can also be obtained through trading between players.",
+      img: "https://static.wikia.nocookie.net/rocketleague/images/2/2e/Fennec_body_icon.png/revision/latest?cb=20190704172946"
+    },
+    {
+      title: "Twinzer",
+      desc: "Twinzer is a battle-car released on May 29, 2018. It can only be obtained from the Impact Crate, and is classed as Import.",
+      img: "https://static.wikia.nocookie.net/rocketleague/images/b/b1/Twinzer_body_icon.png/revision/latest?cb=20180602163611"
+    },
+    {
+      title: "Battle Bus",
+      desc: "The Battle Bus is a Battle-Car which could be obtained from the Llama-Rama event. It is classified as Limited rarity.",
+      img: "https://static.wikia.nocookie.net/rocketleague/images/b/bf/Battle_Bus_body_icon.png/revision/latest?cb=20200927000958"
+    },
+  ];
+  
